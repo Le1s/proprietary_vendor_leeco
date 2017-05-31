3b8e01c98434a843a60741e91b63b6856a1a8876
Sahil Soni <sscsps@gmail.com>
2017-05-31 20:31:06 +0530

    x3 added from wisniaPL's repo
    check github/com/WisniaPL for his repo

---
+++ x3/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+$(info [Decker] copying DP Framework proprietary blobs)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = $VENDOR
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = proprietary/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
+include $(BUILD_PREBUILT)
+
