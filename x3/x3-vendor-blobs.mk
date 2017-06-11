51c43c671377c405259d8d934ab9007e38a062a1
Sahil Soni <sscsps@gmail.com>
2017-06-11 16:57:09 +0530

    replaced all PRODUCT_COPY_FILES with BUILD_PREBUILT

--- x3/x3-vendor-blobs.mk
+++ x3/x3-vendor-blobs.mk
@@ -1,15 +1,13 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/lib,system/lib)
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/lib64,system/lib64)
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/bin,system/bin)
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/etc,system/etc)
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/xbin,system/xbin)
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,system/vendor)
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app,system/app)
-
-
+BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app,system/app)
