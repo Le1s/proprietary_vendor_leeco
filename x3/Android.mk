931e0066078b35f3845f6d43e511250763e1603d
Sahil Soni <sscsps@gmail.com>
2017-06-13 18:45:56 +0530

    using BUILD_PREBUILT for apk files

--- x3/Android.mk
+++ x3/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-$(info [Decker] copying DP Framework proprietary blobs)
 
+$(info [Decker] copying DP Framework proprietary blobs)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE = libdpframework
@@ -23,3 +23,41 @@ LOCAL_MULTILIB = 32
 LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
+
+$(info [SscSPs] copying APK files)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GFManager
+LOCAL_MODULE_OWNER := Letv
+LOCAL_SRC_FILES := proprietary/app/GFManager/GFManager.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := LetvRemoteControl_preinstall
+LOCAL_MODULE_OWNER := Letv
+LOCAL_SRC_FILES := proprietary/app/LetvRemoteControl_preinstall/LetvRemoteControl_preinstall.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := UEIQuicksetSDKLeTV
+LOCAL_MODULE_OWNER := Letv
+LOCAL_SRC_FILES := proprietary/app/UEIQuicksetSDKLeTV/UEIQuicksetSDKLeTV.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
--- x3/x3-vendor-blobs.mk
+++ x3/x3-vendor-blobs.mk
@@ -10,4 +10,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietar
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,system/vendor)
 
-BUILD_PREBUILT += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app,system/app)
+#app folder files
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app/LetvRemoteControl_preinstall/lib,system/app/LetvRemoteControl_preinstall/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app/mcRegistry,system/app/mcRegistry)
