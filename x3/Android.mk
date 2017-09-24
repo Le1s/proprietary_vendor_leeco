dda850b6f7f8a304368c0aa6e2104ae772c984ac
Sahil Soni <sscsps@gmail.com>
2017-09-25 01:15:31 +0530

    Add libnvram module

--- x3/Android.mk
+++ x3/Android.mk
@@ -15,6 +15,17 @@ LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
 LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnvram
+LOCAL_SRC_FILES_64 := proprietary/lib64/libnvram.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libnvram.so
+LOCAL_SHARED_LIBRARIES := libcustom_nvram libnvram_platform libnvram_sec libstdc++
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
 
 $(info [SscSPs] copying APK files)
 
