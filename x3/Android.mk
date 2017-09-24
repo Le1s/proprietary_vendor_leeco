a791d80825cc2b03a0c8e474f1fa4b16d73cfa5f
Sahil Soni <sscsps@gmail.com>
2017-09-25 01:15:03 +0530

    Unify libdpframework module

--- x3/Android.mk
+++ x3/Android.mk
@@ -3,24 +3,16 @@ LOCAL_PATH := $(call my-dir)
 
 $(info [Decker] copying DP Framework proprietary blobs)
 
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_OWNER = $VENDOR
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = proprietary/lib64/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE := libdpframework
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_OWNER = mtk
-LOCAL_MODULE_SUFFIX = .so
+LOCAL_MODULE_SUFFIX := .so
 LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
 
