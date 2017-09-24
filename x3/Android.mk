4fd1ec016e1d7c050ce1ed5bbc20acb38ed21486
Sahil Soni <sscsps@gmail.com>
2017-09-25 01:21:36 +0530

    Remove extra local_path

--- x3/Android.mk
+++ x3/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 
 $(info [Decker] copying DP Framework proprietary blobs)
 
-LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -15,7 +14,6 @@ LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
 LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
-LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libnvram
 LOCAL_SRC_FILES_64 := proprietary/lib64/libnvram.so
