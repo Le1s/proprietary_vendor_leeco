ef227b8e55f92e90bf82670ffeab11a5aa12c06f
SuryashankarDas <suryashankardas.2002@gmail.com>
2017-10-22 19:34:21 +0530

    rename

---
+++ X3/X3-vendor-blobs.mk
@@ -0,0 +1,23 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/vendor,system/vendor)
+
+# Files from app folder
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/app/LetvRemoteControl_WW/lib,system/app/LetvRemoteControl_WW/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeEco/X3/proprietary/app/mcRegistry,system/app/mcRegistry)
+
+# App's APKs
+PRODUCT_PACKAGES += \
+	GFManager \
+	LetvRemoteControl_WW \
+	UEIQuicksetSDKLeTV \
+	LocationEM2 \
+	MTKThermalManager
