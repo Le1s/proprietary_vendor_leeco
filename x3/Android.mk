479527cf8ca0eebdf2c8e6f05d989877098ae4f5
vishalk17 <vishalkapadi95@gmail.com>
2019-02-05 18:12:52 +0000

    x3:Android.bp/.mk : move defined shared libraries to vendor

--- x3/Android.mk
+++ x3/Android.mk
@@ -28,6 +28,7 @@ LOCAL_MODULE = libcam.utils.sensorlistener
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_OWNER = mtk
 LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libcam.utils.sensorlistener.so
 LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libcam.utils.sensorlistener.so
@@ -37,6 +38,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libion_mtk
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libion_mtk.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libion_mtk.so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -46,6 +48,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libged
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libged.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libged.so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -55,6 +58,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := librilmtk
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/librilmtk.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/librilmtk.so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -64,6 +68,17 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := mtk-ril
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/mtk-ril.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/mtk-ril.so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgf_hal
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libgf_hal.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libgf_hal.so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
