49b0a912d53c725aea103778b2eff33ab8a715b4
vishalk17 <vishalkapadi95@gmail.com>
2019-03-09 23:50:52 +0530

    Android.bp: For camera patch this should be define here libdpframework.so

--- x3/Android.mk
+++ x3/Android.mk
@@ -4,26 +4,6 @@ LOCAL_PATH := $(call my-dir)
 $(info [Decker] copying DP Framework proprietary blobs)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_OWNER = $VENDOR
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_OWNER = mtk
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE = libcam.utils.sensorlistener
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_OWNER = mtk
